dfebd131eb04d001ab27adb007aa9ce11caf09ea
Nick801218 <nick757416@gmail.com>
2019-12-29 18:13:01 +0800

    change waveform

--- digital_signal/waveform.py
+++ digital_signal/waveform.py
@@ -2,7 +2,7 @@ from scipy.io.wavfile import read
 import matplotlib.pyplot as plt
 
 #filename = input("D:\python_venv\practice-venv\DSP python practice\digital_signal\handel.wav")
-sampling_rate,x = read("D:\\DSP-with-python\\digital_signal\\handel.wav")
+sampling_rate,x = read("D:\\DSP-with-python\\digital_signal\\fart.wav")
 #read("欲開啟檔案位置") 重要
 print(sampling_rate)
 plt.plot(x)
